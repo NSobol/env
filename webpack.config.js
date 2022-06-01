c98f1000c7533e67e50c0b3fde2f4c1c5b06c88f
Soboleva Natalia <nsoboleva1979@rambler.ru>
2022-06-01 20:45:58 +0300

    Change files

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-let mode = 'development';
-if (process.env.NODE_ENV === 'production') {
-  mode = 'production';
-}
-
-module.exports = {	
-  entry: './src/index.js',
+module.exports = {
+  target: 'web',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-	filename: '[name].bundle.js',
-	assetModuleFilename: 'assets/images/[name]-[hash][ext]'
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, '/dist'),
+    open: true,
+    compress: true,
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-    },
-    {
-      test: /\.html$/,
-      use: [{
-        loader: 'html-loader',
-      }],
-    },
-    {
-      test: /\.css$/,
-      use: [
-        MiniCssExtractPlugin.loader, 'css-loader',
-      ],
-		  },
-	{
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-    },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
     ],
   },
   plugins: [
@@ -46,7 +46,9 @@ module.exports = {
       filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles.css',
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
