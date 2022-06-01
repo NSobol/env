8f8a41fac27a86535cb50bbed5ce899b3ffc6870
Soboleva Natalia <nsoboleva1979@rambler.ru>
2022-06-01 23:34:43 +0300

    Change files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
 
 module.exports = {	
   entry: './src/index.js',
