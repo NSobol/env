47a8fbd005c432e63042d48e109875b6da3bda8b
Soboleva Natalia <nsoboleva1979@rambler.ru>
2022-06-02 12:39:42 +0300

    Внесла исправления

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+/*const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -50,4 +50,4 @@ module.exports = {
       filename: 'styles.css',
     }),
   ],
-};
+};*/
